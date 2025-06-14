a67da12e058269e67db04fff443b650f85d7f138
Raj Joshi <raj.joshi@sentry.io>
2025-06-14 09:24:34 -0700

    :sparkles: feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <html lang="en">
+            <head>
+                <title>Sentry Error Generator</title>
+                <meta name="description" content="Send sample errors to your Sentry project" />
+            </head>
+            <body>
+                <ChakraProvider theme={theme}>{children}</ChakraProvider>
+            </body>
+        </html>
+    );
+};
+
+export default RootLayout;
---
+++ instrumentation-client.ts
@@ -0,0 +1,30 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://f4b6af72d1a9ba9933dcc2e2d45ba4b1@o1.ingest.us.sentry.io/4509497897648128',
+
+  integrations: [
+    Sentry.replayIntegration(),
+    Sentry.feedbackIntegration({
+      colorScheme: 'system',
+    }),
+    Sentry.browserTracingIntegration(),
+  ],
+
+  tracesSampleRate: 1,
+  
+  sendDefaultPii: true,
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+
+  environment: process.env.NODE_ENV,
+
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
